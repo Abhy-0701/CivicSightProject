af10f600c6d86b328998ca5c87a6ba595b83a9b2
Abhyoday_Rathore <ishurathore0000@gmail.com>
2026-06-24 11:46:31 +0530

    Some Formatting of the folder

--- reverseGeoEncoding/test.py
+++
@@ -1,8 +0,0 @@
-import requests, json
-
-url = "https://nominatim.openstreetmap.org/reverse"
-headers = {"User-Agent": "CivicSightProject/1.0(ishurathore0000@gmail.com)"}
-params = {"format": "jsonv2", "lat": 28.701220, "lon": 77.270935, "addressdetails": 1}
-
-r = requests.get(url, params=params, headers=headers, timeout=10)
-print(json.dumps(r.json().get("address", {}), indent=2))
